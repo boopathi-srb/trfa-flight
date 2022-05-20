f6f3958c49f48d05772a4990785722fcd55373ee
Rathina Boopathi <rath.1911180@gct.ac.in>
2022-05-20 22:24:53 +0530

    name changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import One from '../components/one/One'
-
-
+import Banner from '../Assets/Travel Fika banner.png'
+import '../../src/App.css'
 function Home() {
   return (
-    <div>
+    <div className='home'>
+      <div className='banner'>
+         <img src={Banner} alt="banner" />
+      </div>
       <One/>
     </div>
   )
