4e56a4a8f03dee129090aed34b06a290811dd786
Rathina Boopathi <rath.1911180@gct.ac.in>
2022-05-21 13:20:30 +0530

    name changes

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './App.css';
-// import logo from "../../images/logo-NT.png";
+import logo from "./Assets/logo.png";
 import AOS from 'aos';
 import "aos/dist/aos.css"
 
@@ -21,26 +21,24 @@ function close(){
     <div>
      <div className="topbar">
        <div className="logo_wrapper">
-          {/* <img  classname="logo" src={logo} alt="logo"/>   */}
-          <h1>Travel Fika</h1>
+          <img  classname="logo" src={logo} alt="logo"/>    
        </div>
-      
-      <div className="rightside">
-       <input type="checkbox" id="toggle_button" className="toggle_button" aria-controls="nav" aria-expanded="false">
-       </input>
-      
-       <nav className="topbar_links close" id="nav"  >
-           
-             <NavLink onClick={close}  class="link" id="links" activeclassname="active" exact to="/signup">Sign-Up</NavLink>
-             <NavLink onClick={close}  class="link" id="links" activeclassname="active" exact to="/login">Log-in</NavLink>
-            
-       </nav>
-       <label for="toggle_button" className="toggle_button_label">
-         <span></span>
-         <p>X</p>
-       </label>
+       <div className="brand-name">
+         <h1>Travel Fika</h1>
+       </div>
+       <div className="rightside">
+         <input type="checkbox" id="toggle_button" className="toggle_button" aria-controls="nav" aria-expanded="false">
+         </input>
+         <nav className="topbar_links close" id="nav"  >
+            <NavLink onClick={close}  class="link" id="links" activeclassname="active" exact to="/signup">Sign-Up</NavLink>
+            <NavLink onClick={close}  class="link" id="links" activeclassname="active" exact to="/login">Log-in</NavLink>
+         </nav>
+         <label for="toggle_button" className="toggle_button_label">
+           <span></span>
+           <p>X</p>
+         </label>
+        </div>
       </div>
-     </div>
     </div>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import One from '../components/One'
 import Banner from '../Assets/Travel Fika banner.png'
+import plane from '../Assets/plane.svg'
+import hotel from '../Assets/hotel.svg'
+import cab from '../Assets/taxi.svg'
 import '../../src/App.css'
 function Home() {
   return (
@@ -11,10 +14,25 @@ function Home() {
       <div className='greetings'>
       <h2>Let's book your next trip! </h2>
       </div>
-      <div className='one'>
+      <div className='options-wrapper'>
+        <div className='options'>
+         <img src={plane} alt='Plane'/>
+         <h4>Flights</h4>
+        </div>
+        <div className='options'>
+         <img src={hotel} alt='hotel'/>
+         <h4>Hotels</h4>
+        </div>
+        <div className='options'>
+         <img src={cab} alt='cab'/>
+         <h4>Cabs</h4>
+        </div>
+
+      </div>
+      {/* <div className='one'>
        
        <One/>
-      </div>
+      </div> */}
      
     </div>
   )
